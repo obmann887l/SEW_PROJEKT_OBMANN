094c1923979777f261e030cfe0b2a847691b4a58
obmann887l <34032198+obmann887l@users.noreply.github.com>
2018-05-14 15:52:05 +0200

    Second commit

--- Test/MainWindow.xaml.cs
+++ Test/MainWindow.xaml.cs
@@ -17,9 +17,6 @@ using System.Collections.ObjectModel;
 
 namespace Test
 {
-    /// <summary>
-    /// Interaktionslogik für MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
 
